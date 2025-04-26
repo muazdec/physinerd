2df197435ec1320ce094dd405f9051de370e3085
muazdec <wudumuaz@gmail.com>
2025-04-26 10:08:15 +0300

    Update pop.py

--- pop.py
+++ pop.py
@@ -1,12 +1,5 @@
 import streamlit as st
 import time
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
-import tensorflow.keras as tk
-import seaboarn as sn
-import math
-import scipy.constants as sc
 
 
 # Custom CSS for animations, colors, and UI styling
